c805fbef6f6b0b14fb6f5e606670a9475f27145b
Oscarwasoccupied <42104387+Oscarwasoccupied@users.noreply.github.com>
2026-02-19 07:55:33 +0000

    Deploying to gh-pages from @ Oscarwasoccupied/Oscarwasoccupied.github.io@f73401c68faa24a13a29837b9873006f33b5f223 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -472,6 +472,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/xianweizou", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
