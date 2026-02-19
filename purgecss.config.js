f6cfd97f5b5fb5e9a1463f49b422dec2cf9ce270
oscarwasoccupied <z_xw99@outlook.com>
2026-02-19 00:06:30 -0800

    fix linkeIn icon

--- purgecss.config.js
+++ purgecss.config.js
@@ -3,4 +3,6 @@ module.exports = {
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
   skippedContentGlobs: ["_site/assets/**/*.html"],
+  // Keep social icon classes that jekyll-socials injects (plugin output may not be seen by PurgeCSS in some builds)
+  safelist: ["fa-brands", "fa-linkedin", "fa-github", "fa-google-scholar", "ai-google-scholar"],
 };
